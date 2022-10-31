9686f1643ab6aed1034ca217870c348bf02cadc3
YunGil Jun <gilcreativity@gmail.com>
2022-11-01 01:46:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 
-view = [100, 150, 30]
-view
-st.write('## bar Chart')
-st.bar_chart(view)
+with st.sidebar:
+    st.image("https://static.wixstatic.com/media/4360de_5d12982365d74b2c8a19837c31c212ef~mv2.png/v1/fill/w_380,h_82,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4360de_5d12982365d74b2c8a19837c31c212ef~mv2.png")
+    st.title("Data Analysis")
+    choice = st.radio("Navigation", ["Upload", "Profiling", "ML", "Download"])
+    st.info("This application allows you to test an automated ML pipeline using Streamlit")
+
+st.write("Hello world!")
